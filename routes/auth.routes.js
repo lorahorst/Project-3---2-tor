0b835a12e283ecc8fb528d0e922d56cbe742fccd
elmehdi rhindi <g6handi@gmail.com>
2022-04-05 19:01:54 +0200

    first commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const User = require("../models/User.model");
+
+const router = express.Router();
+
+// signup route
+
+
+// login route
+
+module.exports = router;
